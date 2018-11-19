9981ec8cad5c9ad21b9a1b71a865d70091a69768
Maxim Alov <maxim@aludio.com>
2018-11-19 17:41:11 +0300

    Added basic layout

--- app/src/main/kotlin/com/orcchg/quotes/presentation/QuotesActivity.kt
+++ app/src/main/kotlin/com/orcchg/quotes/presentation/QuotesActivity.kt
@@ -1,7 +1,15 @@
 package com.orcchg.quotes.presentation
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
+import com.orcchg.quotes.R
 
 class QuotesActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+        val viewModel = ViewModelProviders.of(this).get(QuotesViewModel::class.java)
+    }
 }
