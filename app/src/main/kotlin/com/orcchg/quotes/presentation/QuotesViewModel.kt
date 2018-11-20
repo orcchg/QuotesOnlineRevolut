36d54f4f5ae0f9e7c514e89bdfd700321a2173e8
Maxim Alov <maxim@aludio.com>
2018-11-20 12:52:28 +0300

    Make request to quotes from viewmodel

--- app/src/main/kotlin/com/orcchg/quotes/presentation/QuotesViewModel.kt
+++ app/src/main/kotlin/com/orcchg/quotes/presentation/QuotesViewModel.kt
@@ -2,9 +2,31 @@ package com.orcchg.quotes.presentation
 
 import androidx.lifecycle.ViewModel
 import com.orcchg.quotes.data.Cloud
+import com.orcchg.quotes.domain.Quotes
 import com.orcchg.quotes.presentation.adapter.QuotesAdapter
+import io.reactivex.disposables.Disposable
+import timber.log.Timber
 
-class QuotesViewModel(cloud: Cloud) : ViewModel() {
+class QuotesViewModel(private val cloud: Cloud) : ViewModel() {
 
     val adapter = QuotesAdapter()
+    private var disposable: Disposable? = null
+
+    override fun onCleared() {
+        super.onCleared()
+        clear()
+    }
+
+    fun quotes(base: String = "USD") {
+        clear()
+        disposable = cloud.quotes(base).subscribe(this::stateQuotesLoaded, Timber::e)
+    }
+
+    private fun clear() {
+        disposable?.dispose()
+    }
+
+    private fun stateQuotesLoaded(quotes: Quotes) {
+//        adapter.models = quotes.rates.map {}
+    }
 }
